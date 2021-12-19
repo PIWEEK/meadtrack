0302eaf5af411f7072070731a1fa86e36e8a0c5c
Pablo Ruiz-Múzquiz <diacritica@gmail.com>
2021-12-19 20:39:43 +0100

    url

--- cms/config/server.js
+++ cms/config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: 'https://merrybrew.app',
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '2688ca5aa2bd0e138f7a8c63532b693e'),
